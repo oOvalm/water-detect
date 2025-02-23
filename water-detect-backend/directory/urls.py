5cb8910ed7836af80e89bf42a1dba96ee4c0f978
oval_m <1332256234@qq.com>
2025-02-24 01:47:02 +0800

    tmp

--- water-detect-backend/directory/urls.py
+++ water-detect-backend/directory/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from directory.views import VideoView, FileListView
+from directory.views import VideoView, FileListView, FolderListView
 
 urlpatterns = [
     path('video', VideoView.as_view(), name='video'),
     path('fileList', FileListView.as_view(), name='FileList'),
+    path('folderList', FolderListView.as_view(), name='FolderList'),
 ]
